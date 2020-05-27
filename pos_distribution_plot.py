862d78b5eb628a41cc07204eca5df4b666915a29
MillerMC96 <pywang@ucdavis.edu>
2020-05-26 21:05:31 -0700

    added find deviation function

--- pos_distribution_plot.py
+++ pos_distribution_plot.py
@@ -6,15 +6,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+def find_position_deviation(p0, pn):
+    dx = pn[0] - p0[0]
+    dy = pn[1] - p0[1]
+    dz = pn[2] - p0[2]
+
 # read file from command line
 coor_buffer_file = sys.argv[1]
 
 coor_buffer = open(coor_buffer_file, 'r')
 lines = coor_buffer.readlines()
 
+# initial position of atom
+first_entry = lines[0].split()
+init_position = [first_entry[5], first_entry[6], first_entry[7]]
+
 for line in lines:
     line_entry = line.split()
     # skip frame index
     if line_entry[0] != "frame":
-        # print coordinates
-        print(line_entry[5], line_entry[6], line_entry[7])
+        # process data
