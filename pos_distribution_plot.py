bb1feaae046f004877739d1308e9c2b78d87bb3d
MillerMC96 <pywang@ucdavis.edu>
2020-05-26 20:56:52 -0700

    processing coordinates

--- pos_distribution_plot.py
+++ pos_distribution_plot.py
@@ -9,4 +9,12 @@ import sys
 # read file from command line
 coor_buffer_file = sys.argv[1]
 
-coor_buffer = open(coor_buffer_file)
+coor_buffer = open(coor_buffer_file, 'r')
+lines = coor_buffer.readlines()
+
+for line in lines:
+    line_entry = line.split()
+    # skip frame index
+    if line_entry[0] != "frame":
+        # print coordinates
+        print(line_entry[5], line_entry[6], line_entry[7])
