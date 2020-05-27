4721cfce8b62068466697009d9b82b3c7b0d688e
MillerMC96 <pywang@ucdavis.edu>
2020-05-26 18:09:31 -0700

    initialize with two scripts

---
+++ extract_frames.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+#frame buffer
+fb=framebuffer.pdb
+
+#xtc file
+tr=$1
+
+#gromacs
+gro=$2
+
+#coordinate buffer
+cb=$3
+START=0
+
+#frame count
+END=$(($4 * 2))
+#calculating half steps
+#decimal places
+DP="scale=2;"
+
+#time step
+TS="*0.5"
+
+#headgroup carbon atom number
+ATOM_NUM=329911
+
+for (( i=$START; i<$END; i++ ))
+do
+    TIME=$(echo "$DP$i$TS" | bc)
+    echo 1 | gmx trjconv -f $tr -s $gro -dump $TIME -o $fb &>/dev/null 
+
+    #frame count
+    fc=$((i+1))
+    echo "frame $fc: " >> $cb
+
+    #find the carbon attached to the tail
+    grep '$ATOM_NUM' $fb >> $cb
+
+    #remove framebuffer
+    rm $fb 
+done
+
---
+++ pos_distribution_plot.py
@@ -0,0 +1,4 @@
+# this script is used for plotting the distribution of the position of a chosen
+# atom
+# Author: Panyue Wang (pywang@ucdavis.edu)
+
