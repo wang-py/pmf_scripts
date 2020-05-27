369bcfbff19af81c7388c201cc6e56a262400ef4
MillerMC96 <pywang@ucdavis.edu>
2020-05-27 13:11:03 -0700

    assigning individual xlabels

--- pos_distribution_plot.py
+++ pos_distribution_plot.py
@@ -47,14 +47,14 @@ for line in lines:
 fig, ax = plt.subplots(1, 3, sharey = True, sharex = True, figsize=(10,6))
 bins = 20
 fig.suptitle("atom movement distribution in three directions")
-common_xlabel = "deviation from initial position [Å]"
+# common_xlabel = "deviation from initial position [Å]"
 fig.text(0.5, 0.04, common_xlabel, ha='center')
 # distribution of dx
 ax[0].set(ylabel = "Frequency")
-plot_one_dist(ax[0], bins, dx_arr, 0, save = False)
+plot_one_dist(ax[0], bins, "x", dx_arr, 0, save = False)
 # distribution of dy
-plot_one_dist(ax[1], bins, dy_arr, 0, save = False)
+plot_one_dist(ax[1], bins, "y", dy_arr, 0, save = False)
 # distribution of dz
-plot_one_dist(ax[2], bins, dz_arr, 0, save = False)
+plot_one_dist(ax[2], bins, "z", dz_arr, 0, save = False)
 
 plt.show()
