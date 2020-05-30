eb76ca1da18697c579cad0f280fa9049eea017b2
MillerMC96 <pywang@ucdavis.edu>
2020-05-30 12:24:56 -0700

    re-wrote data entry

--- pos_distribution_plot.py
+++ pos_distribution_plot.py
@@ -23,37 +23,35 @@ init_position_str = np.array([first_entry[1], first_entry[2], first_entry[3]])
 init_position = init_position_str.astype(np.float)
 
 # data arrays for analysis
-dx_arr = []
-dy_arr = []
-dz_arr = []
+x_arr = []
+y_arr = []
+z_arr = []#
 
 for line in lines:
     line_entry = line.split()
     # skip comments
     first_charactor = line_entry[0]
     if first_charactor != "#" | first_charactor != "@":
-        # process data
-        curr_position_str = np.array([line_entry[1], \
-        line_entry[2], line_entry[3]])
-        curr_position = curr_position_str.astype(np.float)
-        # find deviations in three dimensions
-        dx, dy, dz = find_position_deviation(init_position, curr_position)
-        dx_arr.append(dx)
-        dy_arr.append(dy)
-        dz_arr.append(dz)
+        # read data
+        x_arr.append(line[1])
+        y_arr.append(line[2])
+        z_arr.append(line[3])
+
+# numpy array of frames
+frames = np.array(x_arr, y_arr, z_arr)
 
 # plotting
-fig, ax = plt.subplots(1, 3, sharey = True, sharex = True, figsize=(10,6))
-bins = 50
-fig.suptitle("atom movement distribution in three directions")
+#fig, ax = plt.subplots(1, 3, sharey = True, sharex = True, figsize=(10,6))
+#bins = 50
+#fig.suptitle("atom movement distribution in three directions")
 # common_xlabel = "deviation from initial position [Å]"
 # fig.text(0.5, 0.04, common_xlabel, ha='center')
 # distribution of dx
-ax[0].set(ylabel = "Frequency")
-plot_one_dist(ax[0], bins, "x", dx_arr, 0, save = False)
+#ax[0].set(ylabel = "Frequency")
+#plot_one_dist(ax[0], bins, "x", dx_arr, 0, save = False)
 # distribution of dy
-plot_one_dist(ax[1], bins, "y", dy_arr, 0, save = False)
+#plot_one_dist(ax[1], bins, "y", dy_arr, 0, save = False)
 # distribution of dz
-plot_one_dist(ax[2], bins, "z", dz_arr, 0, save = False)
+#plot_one_dist(ax[2], bins, "z", dz_arr, 0, save = False)
 
-plt.show()
+#plt.show()
