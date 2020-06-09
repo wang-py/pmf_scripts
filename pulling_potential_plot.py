5c5a4f9d96a9bc30e39073f8a5ae2121893023df
MillerMC96 <pywang@ucdavis.edu>
2020-06-09 14:10:22 -0700

    added moving mean

--- pulling_potential_plot.py
+++ pulling_potential_plot.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import sys
 
 # read file from command line
@@ -39,8 +40,18 @@ for line in lines:
         force.append(float(line_entry[1]))
         time.append(float(line_entry[0]))
 
+# window
+N = 200
+
+# moving mean
+move_mean = np.convolve(force, np.ones((N,))/N, mode = 'same')
+
+# moving standard deviation
+force_pd = pd.Series(force)
+move_std = force_pd.rolling(N).std()
+
 # energy calculation
-energy_one_sum = np.array(force) * dt
+energy_one_sum = move_mean * dt * velocity
 energy = np.cumsum(energy_one_sum)
 
 # pull force and pulling energy
@@ -49,11 +60,15 @@ fig.suptitle("pulling force and energy along the trajectory " + fig_title)
 
 # pull force
 ax[0].scatter(time, force, s = 2)
+# plotting moving mean
+ax[0].plot(time[N-1:-N], move_mean[N-1:-N], 'r', \
+           label = "moving average over " + str(N*dt) + " ps")
 ax[0].set(ylabel = "Force [kJ/mol/nm]")
 
 # pull energy
-ax[1].scatter(time, energy, s = 2)
+ax[1].scatter(time[N-1:-N], energy[N-1:-N], s = 2)
 ax[1].set(ylabel = "Energy [kJ/mol]")
 ax[1].set(xlabel = "time [ps]")
 
+ax[0].legend(loc = 'best')
 plt.show()
