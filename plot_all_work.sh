e02815125406c97b26056666fc1112ef32b2d1d6
MillerMC96 <pywang@ucdavis.edu>
2020-06-13 10:18:29 -0700

    now files are labelled properly

---
+++ plot_all_work.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# this script plots all displacement distributions and saves those figures
+
+# plotting script
+PLOT_SCRIPT=$1
+
+# folder that contains all xvgs
+RUNS=$2
+
+# pulling velocity
+VELOCITY=$3
+
+# file index
+I=1
+
+for run in `ls $RUNS/*.xvg | sort -V`; do
+  # get rid of parent paths
+  name=$(basename $run)
+
+  # get rid of extensions
+  filename=run_${I}
+
+  python $PLOT_SCRIPT $run $VELOCITY $filename
+  let I=I+1
+  mv $filename.png $RUNS
+
+done
