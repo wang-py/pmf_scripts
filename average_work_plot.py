604c78e82472be42ce61c4bbafaa2611f4b253a2
MillerMC96 <pywang@ucdavis.edu>
2020-06-13 11:46:56 -0700

    folder looping done

--- average_work_plot.py
+++ average_work_plot.py
@@ -5,6 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import os
 from pulling_potential_plot import *
 
+xvg_folder = sys.argv[1]
 
+directory = os.fsencode(xvg_folder)
+
+for file in os.scandir(directory):
+     filename = os.fsdecode(file)
+     # only reads xvgs
+     if filename.endswith(".xvg"): 
+         one_xvg = os.path.join(xvg_folder, filename)
