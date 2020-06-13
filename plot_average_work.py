bd9e8c756b37ff21aa9576cca19bf612a2d6f501
MillerMC96 <pywang@ucdavis.edu>
2020-06-13 11:18:55 -0700

    new script for plotting average work

---
+++ plot_average_work.py
@@ -0,0 +1,10 @@
+# this script is used for plotting the pulling potential along the trajectory
+# Author: Panyue Wang (pywang@ucdavis.edu)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+from pulling_potential_plot import *
+
+
