ef0fd2ef78cfd2e1cebec41c669a3a3d4cefa837
MillerMC96 <pywang@ucdavis.edu>
2020-06-13 14:44:05 -0700

    updating work runs

--- average_work_plot.py
+++ average_work_plot.py
@@ -12,8 +12,30 @@ xvg_folder = sys.argv[1]
 
 directory = os.fsencode(xvg_folder)
 
+def get_one_work(one_xvg, velocity):
+    one_run = open(one_xvg, 'r')
+    lines = one_run.readlines()
+    time, force = read_inputs(lines)
+    # delta time
+    dt = time[1] - time[0]
+
+    # moving mean window
+    time_window = 0.1 #ns or 100 ps
+    N = int(time_window / velocity / dt)
+    move_mean = get_average_force(force, N)
+    work = calculate_work(time, move_mean, velocity)
+
+    return time, work
+
+# data structure that contains all runs
+work_runs = np.array([])
+
+# pulling rate
+velocity = float(sys.argv[2])
+
 for file in os.scandir(directory):
-     filename = os.fsdecode(file)
+    filename = os.fsdecode(file)
      # only reads xvgs
-     if filename.endswith(".xvg"): 
-         one_xvg = os.path.join(xvg_folder, filename)
+    if filename.endswith(".xvg"): 
+        one_work = get_one_work(filename, velocity)
+        work_runs = np.append(work_runs, one_work)
