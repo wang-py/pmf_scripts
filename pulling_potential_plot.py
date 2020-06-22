8b3a0905d85afda31f777aea489652041d972a22
MillerMC96 <pywang@ucdavis.edu>
2020-06-21 23:26:07 -0700

    Q1H2 ver

--- pulling_potential_plot.py
+++ pulling_potential_plot.py
@@ -35,7 +35,7 @@ def get_average_force(force, N):
 # this function finds the time step when searching is over
 def find_end_of_search(mean_force, work):
     #index = np.where(mean_force == np.amax(mean_force))[0][0]
-    index = find_peaks(mean_force, height=150, width=200, distance=200)[0][-1]
+    index = find_peaks(mean_force, height=200, width=200, distance=200)[0][-1]
     total_work = work[index]
     return index, total_work;
 
