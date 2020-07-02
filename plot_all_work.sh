25af5f6c99ff1798ad7e9f2889844900f9c02a6d
MillerMC96 <pywang@ucdavis.edu>
2020-07-02 12:34:04 -0700

    use numbering

--- plot_all_work.sh
+++ plot_all_work.sh
@@ -11,19 +11,27 @@ RUNS=$2
 # pulling velocity
 VELOCITY=$3
 
+# title
+TITLE=$4
+
 # save figures
 SAVE_FIG=1
+
+# run number
+RUN=1
 
 # search work file
 SEARCH_WORK=search_work.txt
 for run in `ls $RUNS/*pullf.xvg | sort -V`; do
-  # get rid of parent paths
-  name=$(basename $run)
+    # get rid of parent paths
+    # name=$(basename $run)
 
-  # get rid of extensions
-  filename=${run%_pullf.xvg}
+    # get rid of extensions
+    # filename=${run%_pullf.xvg}
+    filename=run${RUN}\ ${TITLE}
 
-  python $PLOT_SCRIPT $run $VELOCITY $SAVE_FIG ${filename} >> $SEARCH_WORK
-  #mv $filename.png $RUNS
+    python $PLOT_SCRIPT $run $VELOCITY $SAVE_FIG ${filename} >> $SEARCH_WORK
+    let RUN=$RUN+1
+    #mv $filename.png $RUNS
 
 done
