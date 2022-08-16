375968672dba5999a79a0dbb65fc7e1e85d22355
Panyue Wang <pywang@ucdavis.edu>
2022-08-16 13:57:08 -0700

    update water position

--- generate_pmf_configs_from_tunnel.py
+++ generate_pmf_configs_from_tunnel.py
@@ -14,11 +14,35 @@ def read_tunnel_pdb(tunnel_pdb):
 
 # extract water position from input structure
 def get_water_position(structure_pdb_data):
-    OW = np.array([water.split()[6:9] for water in structure_pdb_data if 'OW' in water]).astype(float)
-    HW1 = np.array([water.split()[6:9] for water in structure_pdb_data if 'HW1' in water]).astype(float)
-    HW2 = np.array([water.split()[6:9] for water in structure_pdb_data if 'HW1' in water]).astype(float)
+    OW = np.array([water.split()[6:9] for water in structure_pdb_data if 'OW' in water]).astype(float)[0]
+    HW1 = np.array([water.split()[6:9] for water in structure_pdb_data if 'HW1' in water]).astype(float)[0]
+    HW2 = np.array([water.split()[6:9] for water in structure_pdb_data if 'HW2' in water]).astype(float)[0]
     
     return OW, HW1, HW2
+
+def format_atom_position(atom_pos):
+    """
+    takes an xyz array and formats it into pdb coordinates
+    """
+    x_pos = str("{0:.3f}".format(atom_pos[0])).rjust(8)
+    y_pos = str("{0:.3f}".format(atom_pos[1])).rjust(8)
+    z_pos = str("{0:.3f}".format(atom_pos[2])).rjust(8)
+    atom_pos_pdb = x_pos + y_pos + z_pos
+
+    return atom_pos_pdb
+
+def update_atom_position(input_str, new_pos):
+    x_i = 30
+    new_pos_pdb = format_atom_position(new_pos)
+    input_str = input_str.replace(input_str[x_i:x_i+24], new_pos_pdb)
+
+    return input_str
+
+def update_water_position(structure_pdb_data, water_i, new_water_pos):
+    for i in range(3):
+        structure_pdb_data[water_i+i] = update_atom_position(structure_pdb_data[water_i+i], new_water_pos[i])
+
+    return structure_pdb_data
 
 # function that takes the tunnel positions and docks water in the input structure
 def dock_one_water(tunnel_point, structure_pdb_data):
@@ -27,11 +51,10 @@ def dock_one_water(tunnel_point, structure_pdb_data):
     delta_pos = tunnel_point - OW
     new_HW1 = HW1 + delta_pos 
     new_HW2 = HW2 + delta_pos 
+    new_water_pos = np.array([tunnel_point, new_HW1, new_HW2])
+    water_i = 6507
 
-    i = 0
-    while i < len(structure_pdb_data):
-        if 'OW' in structure_pdb_data[i]:
-            print("found")
+    structure_pdb_data = update_water_position(structure_pdb_data, water_i, new_water_pos)
 
 
     pass
