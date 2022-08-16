983a8043ca2d7f53b8eb0700551f8e038026db8a
Panyue Wang <pywang@ucdavis.edu>
2022-08-16 14:44:43 -0700

    added docs

--- generate_pmf_configs_from_tunnel.py
+++ generate_pmf_configs_from_tunnel.py
@@ -3,8 +3,11 @@ import sys
 import os
 import numpy as np
 
-# function that reads the tunnel pdb file and return an array of positions
 def read_tunnel_pdb(tunnel_pdb):
+    """
+    function that reads the tunnel pdb file and return an array of positions
+    ----------------------------------------------------------------------------
+    """
     with open(tunnel_pdb, 'r') as tpdb:
         tunnel_data = [line.split()[6:9] for line in tpdb.readlines() if 'ATOM' in line]
     tunnel_data = np.array(tunnel_data)
@@ -12,8 +15,11 @@ def read_tunnel_pdb(tunnel_pdb):
     
     return tunnel_data
 
-# extract water position from input structure
 def get_water_position(structure_pdb_data):
+    """
+    extract water position from input structure
+    ----------------------------------------------------------------------------
+    """
     OW = np.array([water.split()[6:9] for water in structure_pdb_data if 'OW' in water]).astype(float)[0]
     HW1 = np.array([water.split()[6:9] for water in structure_pdb_data if 'HW1' in water]).astype(float)[0]
     HW2 = np.array([water.split()[6:9] for water in structure_pdb_data if 'HW2' in water]).astype(float)[0]
@@ -23,6 +29,7 @@ def get_water_position(structure_pdb_data):
 def format_atom_position(atom_pos):
     """
     takes an xyz array and formats it into pdb coordinates
+    ----------------------------------------------------------------------------
     """
     x_pos = str("{0:.3f}".format(atom_pos[0])).rjust(8)
     y_pos = str("{0:.3f}".format(atom_pos[1])).rjust(8)
@@ -32,6 +39,10 @@ def format_atom_position(atom_pos):
     return atom_pos_pdb
 
 def update_atom_position(input_str, new_pos):
+    """
+    update an atom's position and output it as a pdb string
+    ----------------------------------------------------------------------------
+    """
     x_i = 30
     new_pos_pdb = format_atom_position(new_pos)
     input_str = input_str.replace(input_str[x_i:x_i+24], new_pos_pdb)
@@ -39,13 +50,25 @@ def update_atom_position(input_str, new_pos):
     return input_str
 
 def update_water_position(structure_pdb_data, water_i, new_water_pos):
+    """
+    updates position of water
+    ----------------------------------------------------------------------------
+    """
     for i in range(3):
         structure_pdb_data[water_i+i] = update_atom_position(structure_pdb_data[water_i+i], new_water_pos[i])
 
     return structure_pdb_data
 
-# function that takes the tunnel positions and docks water in the input structure
 def dock_one_water(tunnel_point, structure_pdb_data):
+    """
+    function that takes the tunnel positions and docks water in the input structure
+    ----------------------------------------------------------------------------
+    tunnel_point:
+    structure_pdb_data:
+    ----------------------------------------------------------------------------
+    Returns: structure_pdb_data_new
+    updated structure data with new water position
+    """
     OW, HW1, HW2 = get_water_position(structure_pdb_data)
     # shift hydrogens to new positions as well
     delta_pos = tunnel_point - OW
@@ -58,8 +81,21 @@ def dock_one_water(tunnel_point, structure_pdb_data):
 
     return structure_pdb_data_new
 
-# function that iterate through all tunnel positions and generate pdb configs with water
 def generate_configs(tunnel_points, structure_pdb, prefix_filename):
+    """
+    function that iterates through all tunnel positions and generate pdb configs with water
+    ----------------------------------------------------------------------------
+    tunnel_points: ndarray
+    N x 3 array of xyz positions of all tunnel points
+
+    structure_pdb: string
+    filename of the pdb structure that has the protein with one water
+
+    prefix_filename: string
+    prefix filename of all config files
+
+    ----------------------------------------------------------------------------
+    """
     with open(structure_pdb, 'r') as spdb:
         structure_data = [line for line in spdb.readlines() if 'ATOM' in line]
     
