6c9fb86675a194d72b6c24d662661357f8193d6c
Panyue Wang <pywang@ucdavis.edu>
2022-08-17 17:35:19 -0700

    added functions for work along the path

--- pmf_energy_plot.py
+++ pmf_energy_plot.py
@@ -9,25 +9,55 @@ from glob import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
+def get_data_from_xvg(input_xvg):
+    with open(input_xvg, 'r') as xvg:
+        data_str = [line.split()[1:] for line in xvg if '#' not in line and '@' not in line]
+    
+    data = np.array(data_str).astype(float) * 10
+
+    return data
+
 def get_avg_deviation(input_arr):
     p0 = input_arr[0, :]
     deviations = input_arr - p0
+    mean_deviation = np.zeros(3)
     
-    mean_dx = np.mean(deviations[:, 0])
-    mean_dy = np.mean(deviations[:, 1])
-    mean_dz = np.mean(deviations[:, 2])
+    mean_deviation[0] = np.mean(deviations[:, 0])
+    mean_deviation[1] = np.mean(deviations[:, 1])
+    mean_deviation[2] = np.mean(deviations[:, 2])
 
-    return mean_dx, mean_dy, mean_dz
+    return mean_deviation
+
+def get_force(delta_pos, force_constant):
+    return delta_pos * force_constant
+
+def get_vector(point_A, point_B):
+    return point_B - point_A
+
+def get_work(force, path):
+    return np.dot(force, path)
+
+def get_work_vs_site(tunnel_points, input_xvgs, k):
+    number_of_sites = len(input_xvgs)
+    total_work = []
+    current_work = 0
+    for i in range(number_of_sites - 1):
+        data = get_data_from_xvg(input_xvgs[i])
+        mean_deviation = get_avg_deviation(data)
+        force = get_force(mean_deviation, k)
+        vector = get_vector(tunnel_points[i], tunnel_points[i+1])
+        work = get_work(force, vector)
+        current_work += work
+        total_work.append(total_work)
+    
+    return total_work
+
 
 def get_average_energy(input_xvg, k):
-    with open(input_xvg, 'r') as f:
-        data_str = [line.split()[1:] for line in f if '#' not in line and '@' not in line]
-
-    data = np.array(data_str)
-    data = data.astype(float) * 10 #convert to angstroms
+    data = get_data_from_xvg(input_xvg)
     
-    mean_dx, mean_dy, mean_dz = get_avg_deviation(data)
-    mean_energy = (mean_dx ** 2 + mean_dy ** 2 + mean_dz ** 2) * k / 2
+    mean_deviation = get_avg_deviation(data)
+    mean_energy = (mean_deviation[0] ** 2 + mean_deviation[1] ** 2 + mean_deviation[2] ** 2) * k / 2
 
     return mean_energy
 
@@ -46,7 +76,6 @@ def plot_average_energy_vs_site(input_xvgs, k):
 
 if __name__ == '__main__':
     path = sys.argv[1]
+    force_constant = sys.argv[2]
     pos_files = sorted(glob(path + "/*_water.xvg"), key=os.path.getmtime)
-    k = 10 # kJ/mol/A^2
-    plot_average_energy_vs_site(pos_files, k)
     pass
