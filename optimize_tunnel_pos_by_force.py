b77388b082987ce2ef97e894e47607f144bd6089
Panyue Wang <pywang@ucdavis.edu>
2022-08-28 18:51:24 -0700

    added script for updating tunnel positions

---
+++ optimize_tunnel_pos_by_force.py
@@ -0,0 +1,51 @@
+# script that optimizes the tunnel points by shifting them in the direction 
+# of force exerted by the protein
+from pmf_functions import *
+from generate_pmf_configs_from_tunnel import *
+
+def get_pdb_data(structure_pdb):
+    with open(structure_pdb, 'r') as spdb:
+        box_data = [line for line in spdb.readlines() if 'CRYST' in line][0]
+        spdb.seek(0,0)
+        structure_data = [line for line in spdb.readlines() if 'ATOM' in line]
+    
+    return box_data, structure_data
+
+def get_norm_of_force_vectors(force_vs_site):
+    norm = [np.linalg.norm(x) for x in force_vs_site]
+
+    return norm
+
+def normalize_force_vectors(force_vs_site):
+    max_norm = np.max(get_norm_of_force_vectors(force_vs_site))    
+
+    return force_vs_site / max_norm
+
+def update_tunnel_point_position(tunnel_pdb_data, tunnel_points, force_vs_site, delta_pos):
+    num_of_pts = tunnel_pdb_data.shape[0]
+    normalized_force = normalize_force_vectors(force_vs_site)
+    new_tunnel_pdb_data = []
+    for i in range(num_of_pts):
+        one_tp = tunnel_points[i]
+        one_force = normalized_force[i]
+        shift = delta_pos * one_force 
+        new_tp = one_tp + shift
+        one_tp_string = tunnel_pdb_data[i]
+        one_tp_string = update_atom_position(one_tp_string, new_tp)
+        new_tunnel_pdb_data.append(one_tp_string)
+    
+    new_tunnel_pdb_data = np.array(new_tunnel_pdb_data).astype(str)
+
+    return new_tunnel_pdb_data
+
+if __name__ == "__main__":
+    # pdb file of the tunnel points
+    tunnel_pdb = sys.argv[1]
+    tunnel_points = read_tunnel_pdb(tunnel_pdb)
+    # working directory that contains all water position xvgs
+    pos_folder = sys.argv[2]
+    pos_files = sorted(glob(pos_folder + "/*_water.xvg"), key=os.path.getmtime)
+    # force constant in kJ/mol/A^2
+    k = float(sys.argv[3])
+    force_vs_site = get_force_vs_site(tunnel_points, pos_files, k)
+    pass
