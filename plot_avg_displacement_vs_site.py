ccf4a810f9b67bb42f77f11e6be38faa71c961df
Panyue Wang <pywang@ucdavis.edu>
2022-08-30 13:30:18 -0700

    plot all

--- plot_avg_displacement_vs_site.py
+++ plot_avg_displacement_vs_site.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     # working directory that contains all xvgs
     pos_path = sys.argv[1]
     pos_files = sorted(glob(pos_path + "/*_water.xvg"), key=os.path.getmtime)
-    plot_average_displacement_vs_site(pos_files[:-1])
+    plot_average_displacement_vs_site(pos_files)
