6900dae1f4f9cf88fbc12c5eee8536225b018e6d
Panyue Wang <pywang@ucdavis.edu>
2022-09-08 14:04:48 -0700

    plot work and energy at the same time

--- pmf_energy_plot.py
+++ pmf_energy_plot.py
@@ -1,17 +1,22 @@
 # script that calculates energy from average drift and plots it.
 from pmf_functions import *
 from generate_pmf_configs_from_tunnel import read_tunnel_pdb
+from gromacs_energy_plot import *
 
 if __name__ == '__main__':
     # working directory that contains all xvgs
     path = sys.argv[1]
+    energy_path = path + "/PMF_energy"
     # pdb for tunnel points
     tunnel_pdb = sys.argv[2]
     # force constant in kJ/mol/A^2
     force_constant = float(sys.argv[3])
     pos_files = sorted(glob(path + "/*_water.xvg"), key=os.path.getmtime)
+    energy_files = sorted(glob(energy_path + "/*_energy.xvg"), key=os.path.getmtime)
+    energies = get_energy_vs_site(energy_files)
     tunnel_points = read_tunnel_pdb(tunnel_pdb)
     work_vs_site = get_work_vs_site(tunnel_points, pos_files, force_constant)
-    plot_work_and_total_work(work_vs_site)
-    force_vs_site = get_force_vs_site(tunnel_points, pos_files, force_constant)
+    plot_work_and_energy(work_vs_site, energies, force_constant)
+    #plot_work_and_total_work(work_vs_site)
+    #force_vs_site = get_force_vs_site(tunnel_points, pos_files, force_constant)
     pass
