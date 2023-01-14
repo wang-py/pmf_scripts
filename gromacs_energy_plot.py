5117f13e1b147a8c4ad87c84ac67daa34414697e
Panyue Wang <pywang@ucdavis.edu>
2023-01-13 16:09:56 -0800

    reading the site numbers from the energy files

--- gromacs_energy_plot.py
+++ gromacs_energy_plot.py
@@ -11,6 +11,12 @@ def get_energy_from_xvg(input_xvg):
     data = np.array(data_str).astype(float)
 
     return data
+
+def get_site_number_from_energy_file(energy_file):
+    filename = os.path.basename(energy_file)
+    site_number = float(filename.split('_')[5])
+
+    return site_number
     
 def get_energy_vs_site(energy_files):
     """
@@ -18,18 +24,18 @@ def get_energy_vs_site(energy_files):
     """
     num_of_pts = len(energy_files)
     total_energies = np.zeros(num_of_pts)
+    site_number = np.zeros(num_of_pts)
     for i in range(num_of_pts):
         energy = get_energy_from_xvg(energy_files[i])
+        site_number[i] = get_site_number_from_energy_file(energy_files[i])
         coulomb = energy[:, 0]
         LJ = energy[:, 1]
         avg_total_energy = np.mean(coulomb + LJ)
         total_energies[i] = avg_total_energy
 
-    return total_energies
+    return total_energies, site_number
 
-def plot_energy_vs_site(total_energies, dowser_energies=None):
-
-    sites = np.arange(total_energies.shape[0]) + 1
+def plot_energy_vs_site(total_energies, sites, dowser_energies=None):
     cal_to_joules = 4.1868
     plt.plot(sites, total_energies / cal_to_joules, 'bo', label='gromacs')
     if dowser_energies.any():
@@ -52,8 +58,8 @@ if __name__ == "__main__":
     input_path = sys.argv[1]
     energy_files = sorted(glob(input_path + "/*_energy.xvg"), key=os.path.getmtime)
     dowser_energy_file = input_path + "/dowser_energies.txt"
-    energies = get_energy_vs_site(energy_files)
+    energies, sites = get_energy_vs_site(energy_files)
     dowser_energies= get_dowser_energies(dowser_energy_file)
-    plot_energy_vs_site(energies, dowser_energies)
+    plot_energy_vs_site(energies, sites, dowser_energies)
 
     pass
