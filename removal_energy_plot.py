fd2955c963856031249435fd4e11bd964cc1e535
Panyue Wang <pywang@ucdavis.edu>
2024-01-17 11:09:04 -0800

    script for finding water removal energy

---
+++ removal_energy_plot.py
@@ -0,0 +1,62 @@
+from gromacs_energy_plot import *
+
+def get_initial_cluster_energy(cluster_energy_xvg):
+    """
+    function that gets average energy of water cluster from input xvg
+    """
+    energy = get_energy_from_xvg(cluster_energy_xvg)
+    #coulomb = energy[:, 3]
+    #LJ = energy[:, 4]
+    #avg_total_energy = np.mean(coulomb)
+    #avg_total_energy = np.mean(LJ)
+    coulomb = energy[:, [0,2]]
+    LJ = energy[:, [1,3]]
+    avg_total_energy = np.mean(coulomb + LJ)
+
+    return avg_total_energy
+
+def plot_removal_energy_vs_site(removal_energies, sites, output_filename, dowser_energies):
+    cal_to_joules = 4.1868
+    label_fontsize=16
+    fig, ax = plt.subplots(figsize=(14,7))
+    removal_energies_in_cal = removal_energies / cal_to_joules
+    plt.plot(sites, removal_energies_in_cal, 'g^', label='removal', markersize=10)
+    sites_and_removal_energy = np.zeros((len(sites),2))
+    for i in range(len(sites)):
+        sites_and_removal_energy[i] = [int(sites[i]), removal_energies_in_cal[i]]
+    # printing energy values
+    print("Sites and removal energies: ")
+    print(sites_and_removal_energy)
+    #energy_threshold = -7 #kCal/mol
+    #print(f"water with energies higher than {energy_threshold} kCal/mol:")
+    #print(np.array([x for x in sites_and_removal_energy if x[1] > energy_threshold]))
+    ax.set_xticks(sites)
+    ax.tick_params(axis='x', labelsize=label_fontsize)
+    ax.tick_params(axis='y', labelsize=label_fontsize)
+    if dowser_energies.any():
+        plt.plot(sites, dowser_energies, 'rv', label='dowser', markersize=10)
+    #plt.title("total energy vs site number", fontsize=label_fontsize)
+    bulk_energy = -42 / cal_to_joules
+    plt.axhline(bulk_energy, color='k', linestyle='--', label='energy of water in bulk %.1f kCal/mol'%bulk_energy)
+    plt.xlabel("site number", fontsize=label_fontsize)
+    plt.ylabel("energy [kCal/mol]", fontsize=label_fontsize)
+    plt.legend(loc="best")
+    plt.savefig(output_filename+".png", dpi=200)
+    plt.show()
+    pass
+
+if __name__ == '__main__':
+    cluster_energy_xvg = sys.argv[1]
+    minus_one_energy_path = sys.argv[2]
+    if len(sys.argv) > 3:
+        output_fig_filename = sys.argv[3]
+    else:
+        output_fig_filename = "output_fig"
+    minus_one_energy_files = sorted(glob(minus_one_energy_path + "/*_energy.xvg"), key=os.path.getmtime)
+    dowser_energy_file = input_path + "/dowser_energies.txt"
+    minus_one_energies, sites = get_energy_vs_site(minus_one_energy_files)
+    initial_cluster_energy = get_initial_cluster_energy(cluster_energy_xvg)
+    removal_energies = minus_one_energies - initial_cluster_energy
+    dowser_energies= get_dowser_energies(dowser_energy_file)
+    plot_removal_energy_vs_site(removal_energies, sites, output_fig_filename, dowser_energies)
+    pass
